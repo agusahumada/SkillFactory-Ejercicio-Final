c9c7796e98d5590104b19ee630362fa8a01a3909
agusahumada <agustinaahumada649@gmail.com>
2021-08-28 00:13:02 -0300

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const router = require('./routes/routes');
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use("/", router())
+
+app.listen(PORT, () => {
+    console.log(`Se encedió el puerto ${PORT}`);
+});
